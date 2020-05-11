03f7be22abf0e263910543c55b12973ea57cf895
mi16030 <tetejesandra@gmail.com>
2020-05-11 21:15:37 +0200

    sredjeni prvi deo da bude objekto i dodat drugi

--- snake.js
+++ snake.js
@@ -1,150 +1,246 @@
-
 const canvas = document.getElementById("game-area");
 const ctx = canvas.getContext("2d");
+const startBtn = document.getElementById("start-button");
+const stopBtn = document.getElementById("stop-button");
+const newGameBtn = document.getElementById("new-game-button");
 
-ctx.fillStyle="#F1F1F1";
-ctx.fillRect(0, 0, 810, 600);
+const height = 640;
+const width = 1280;
+const Directions = Object.freeze({"UP":1, "DOWN":2, "LEFT":3, "RIGHT":4});
+let dir=Directions.UP;
 
-const box = 30;
+const box = 20;
+let score = 0;
+const munch = new Audio("munch3.mp3");
+let img = new Image();
+img.src = "apple-remove.png";
+//img.setColorToAlpha(0, 0);
+let bg = new Image();
+bg.src = "field.jpg";
+ctx.clearRect(0, 0, width, height); 
+ctx.drawImage(bg, 0, 0, width, height);
 
-
-let snake=[];
-snake[0]={
-    x:9*box,
-    y:10*box
-};
-snake[1]={
-    x:9*box,
-    y:11*box
-};
-
-let food=createBait();
-let score=0;
-let dir;
-let ind=false;
-document.addEventListener("keydown", direction);
-
-function createBait() {
+class Snake{
     
-    
-    x=Math.floor(Math.random()*27+1)*box;
-    y=Math.floor(Math.random()*20+1)*box;
-    
-    if(collision({x:x, y:y}, snake)){
-        return createBait();
+    constructor(){
+        
+        this.colLen=64;
+        this.rowLen=32;
+        this.snake = [];
+        this.food = {x:null, y:null};
+        for (let i = 0; i < 4; i++) {
+            this.snake.push({
+                x: (Math.ceil(this.colLen / 2) + i)*box,
+                y: (Math.ceil(this.rowLen / 2))*box
+            });
+        }
+        console.log(this.snake);
+        this.createBait();
+        this.interval = null;
+        this.started=false;
+        this.draw();
+        
     }
     
-    return {x:x, y:y};
+     start(){
+        if(!this.started){
+            this.interval = setInterval(this.move.bind(this), 70);
+            this.started=true;
+        }
+    }
+    
+    stop(){
+        //let self = this;
+        if(this.started){
+            clearInterval(this.interval);
+            this.started=false;
+        }
+    }
+    draw(){
+        let snake = this.snake;
+        
+        ctx.clearRect(0, 0, width, height); 
+        ctx.drawImage(bg, 0, 0, width, height);
+        document.getElementById("score").innerHTML="</br>"+score;
+        
+        for(let i =0; i<snake.length; i++){
+            ctx.fillStyle = "blue";
+            ctx.fillRect(snake[i].x, snake[i].y, box, box);
+            
+            ctx.strokeStyle="black";
+            ctx.strokeRect(snake[i].x, snake[i].y, box, box);
+        }
+        //ctx.fillStyle = "black";
+        //ctx.fillRect(this.food.x, this.food.y, box, box);
+        ctx.drawImage(img, this.food.x, this.food.y, 1.15*box, 1.15*box);
+    }
+    
+    move(){
+        let snake = this.snake;
+       
+        this.draw();
+        
+        let headX=snake[0].x;
+        let headY=snake[0].y;
+        
+        switch(dir){
+            case Directions.UP:
+                headY-=box;
+                break;
+            case Directions.DOWN:
+                headY+=box;
+                break;
+            case Directions.LEFT:
+                headX-=box;
+                break;
+            case Directions.RIGHT:
+                headX+=box;
+                break;
+        }
+        
+        //prepravke ako odemo van granica
+        headX=headX % width;
+        headY=headY % height;
+        if(headX<0){ headX=width;} 
+        if(headY<0){ headY=height;} 
+        
+        // ako pojedemo hranu
+        if(headX === this.food.x && headY === this.food.y){
+            munch.play();
+            score++;
+            console.log(this.food);
+            this.createBait();
+            console.log(this.food);
+        }else{
+            snake.pop();
+        }
+        
+        let newHead = {x: headX, y: headY};
+        
+        // game over ako udarimo u sebe
+        if(collision(newHead,snake)){
+            
+            alert('Izgubili ste! Vas rezultat je: '+score);
+            this.stop();
+        }
+        
+        //ako nismo ni pojeli ni udarili zapravo stavljamo novi head na pocetak niza snake
+        snake.unshift(newHead);   
+    }
+    
+    createBait(){
+        
+        let x=Math.floor(Math.random()*this.colLen+1)*box;
+        let y=Math.floor(Math.random()*this.rowLen+1)*box;
+        console.log({x:x, y:y});
+        
+        if(collision({x:x, y:y}, this.snake)){
+            this.createBait();
+        }else{
+            this.food={x:x, y:y};
+        }
+    }
+    
 }
 
-function direction(event){
-    ind=true;
-    switch(event.key){
-        case "ArrowUp":
-            if(dir!="down"){
-                dir="up";	
-                console.log('gore');
+function getDirection(event){
+        
+        switch(event.key){
+            case "ArrowUp":
+                if(dir!=Directions.DOWN){
+                    dir=Directions.UP;	
+                    console.log('gore');
+                }
+                break;
+            case "ArrowDown":
+                if(dir!=Directions.UP){
+                    dir=Directions.DOWN;	
+                    console.log('dole');
+                }
+                break;
+            case "ArrowLeft":
+                if(dir!=Directions.RIGHT){
+                    dir=Directions.LEFT;
+                    console.log('levo');
+                }
+                break;
+            case "ArrowRight":
+                if(dir!=Directions.LEFT){
+                    dir=Directions.RIGHT;
+                    console.log('desno');
+                }
+                break;
             }
-            break;
-        case "ArrowDown":
-            if(dir!="up"){
-                dir="down";	
-                console.log('dole');
-            }
-            break;
-        case "ArrowLeft":
-            if(dir!="right"){
-                dir="left";
-                console.log('levo');
-            }
-            break;
-        case "ArrowRight":
-            if(dir!="left"){
-                dir="right";
-                console.log('desno');
-            }
-            break;
-		}
-		draw();
-}
+            
+        }
 
 function collision(head,array){
     for(let i = 0; i < array.length; i++){
-        if(head.x == array[i].x && head.y == array[i].y){
+        if(head.x === array[i].x && head.y === array[i].y){
             return true;
         }
     }
     return false;
 }
 
-function draw(){
-    ctx.clearRect(0, 0, 810, 600);
 
-    for(let i =0; i<snake.length; i++){
-        ctx.fillStyle = "blue";
-        ctx.fillRect(snake[i].x, snake[i].y, box, box);
+function loadUsername() {
+    let person = prompt("Please enter your name", "Harry Potter");
+    let username = "";
+
+    if (person == null || person == "") {
+        username = "Unknown";
+    } else {
+        username = person;
+    }
+    
+    return username;
+};
+
+const sendResult = async (username, result) => {
+    try { 
+        const URL = 'http://localhost:3002/';
+        const response = await fetch(URL, {
+            method : 'POST',
+            headers : {
+                'Content-Type': 'application/json'
+            },
+            mode : 'cors',
+            body : JSON.stringify({
+                name : username,
+                score : result
+            })
+        });
+        const jsonResponse = await response.json();
+        console.log(jsonResponse);
         
-        ctx.strokeStyle="black";
-        ctx.strokeRect(snake[i].x, snake[i].y, box, box);
+    } catch (err) {
+        console.error(err);
     }
-    
-    ctx.fillStyle = "black";
-    ctx.fillRect(food.x, food.y, box, box);
-    
-    document.getElementById("score").innerHTML="</br>"+score;
-
-    
-    let oldHeadX=snake[0].x;
-    let oldHeadY=snake[0].y;
-    
-    if(dir=="left") oldHeadX-=box;
-    if(dir=="right") oldHeadX+=box;
-    if(dir=="up") oldHeadY-=box;
-    if(dir=="down") oldHeadY+=box;
-    
-    oldHeadX=oldHeadX % 810;
-    oldHeadY=oldHeadY % 600;
-   
-    if(oldHeadX<0){
-        oldHeadX=810;
-    } 
-    if(oldHeadY<0){
-        oldHeadY=600;
-    } 
-
-    
-    // ako pojedemo hranu
-    if(oldHeadX == food.x && oldHeadY == food.y){
-        score++;
-        food = createBait();
-        ctx.fillStyle = "black";
-        ctx.fillRect(food.x, food.y, box, box);
-        console.log(snake);
-        
-       
-    }else{
-        // skidamo poslednji i ne crtamo ga
-        snake.pop();
-    }
-    
-    
-    
-    let newHead = {
-        x : oldHeadX,
-        y : oldHeadY
-    }
-    
-    // game over
-    
-    if(collision(newHead,snake) && ind){
-        clearInterval(game);
-        alert('izgubili ste');
-    }
-    
-    snake.unshift(newHead);
-    
-    
 }
 
-let game=setInterval(draw, 100);
+const getResult = async () => {
+    try {
+        const URL = 'http://localhost:3002/';
+        const response = await fetch(URL, {
+            method : 'GET',
+            headers : {
+                'Content-Type': 'application/json'
+            }
+        });
+        const jsonResponse = await response.json();
+        console.log(jsonResponse);   
+             
+    } catch (err) {
+        console.log(err);
+    }
+};
+newGameBtn.addEventListener("click", () => {window.location=location.href;});
+let snake = new Snake();
+document.addEventListener("keydown", event => getDirection(event));
+startBtn.addEventListener("click", () => {snake.start();});
+stopBtn.addEventListener("click", () => {snake.stop();});
+
+
+
 
